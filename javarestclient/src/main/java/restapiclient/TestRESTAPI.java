4e5e3ea8b839183404ea21fe20476b7c304c10a8
Parag Shah <parsh78@users.noreply.github.com>
2021-04-28 17:03:15 -0700

    JAVA Rest Client using Unirest library
    https://kong.github.io/unirest-java/#requests

---
+++ javarestclient/src/main/java/restapiclient/TestRESTAPI.java
@@ -0,0 +1,23 @@
+package restapiclient;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+
+public class TestRESTAPI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HttpResponse<String> httpResponse;
+		try {
+			httpResponse = Unirest.get("http://localhost:8080/courses/1").asString();
+			System.out.println( httpResponse.getBody());
+		} catch (UnirestException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			  
+		
+	}
+
+}
